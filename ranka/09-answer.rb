4337ee724280b075522d79197ac7fae8d7c6084c
tosite0345 <tl091264@gmail.com>
2018-10-16 18:26:15 +0900

    09. Typoglycemia

---
+++ ranka/09-answer.rb
@@ -0,0 +1,40 @@
+# rand_char 関数
+# 配列の先頭と末尾を除いたランダムなインデックスを生成する。
+#
+# @author tosite
+# @param  Array   arr
+# @param  Integer max_len
+# @return Array
+# @example
+#   rand_char(%w(aiueo), 4)
+#   # => [2, 3, 1]
+def rand_char(arr, max_len)
+  res = []
+  # puts arr.length
+  for i in 1..(max_len - 1)
+    res.push(i)
+  end
+  return res.shuffle!
+end
+
+buf = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+ans = ''
+arr = buf.split(' ')
+
+arr.each_with_index do |b,i|
+  tmp = ''
+  ans += " "
+  # 文字数が4文字以下なら入れ替えない
+  if b.length <= 4
+    ans += b
+    next
+  end
+  chars   = b.split('')
+  max_len = chars.length - 1
+  rand_char(chars, max_len).each do |idx|
+    tmp += chars[idx]
+  end
+  ans += "#{chars[0]}#{tmp}#{chars[max_len]}"
+end
+
+puts ans
