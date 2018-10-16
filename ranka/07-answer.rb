5a939de1faa7fc0286396563300be7d720a4aa92
tosite0345 <tl091264@gmail.com>
2018-10-16 17:08:19 +0900

    07. テンプレートによる文生成

---
+++ ranka/07-answer.rb
@@ -0,0 +1,2 @@
+require "./lib/template"
+insert_in_wheather_template(12, '気温', 22.4)
---
+++ ranka/lib/template.rb
@@ -0,0 +1,3 @@
+def insert_in_wheather_template(hour = 0, attr = "", value = 0)
+  puts "#{hour}時の#{attr}は#{value}"
+end
